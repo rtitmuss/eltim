bf343ca1cae63bb76591bf53f1f9e4540a66a8a8
Richard Titmuss <richardt@spotify.com>
2022-11-12 10:45:54 +0100

    Refactor to combine kernel and task

--- micropython/kernel.py
+++ micropython/kernel.py
@@ -3,13 +3,13 @@
 # SPDX-License-Identifier: MIT
 
 import machine
+import time
 
 from pimoroni import Button
 
 from clock import Clock
 from display import Display
 import ota
-import task
 import wlan
 
 
@@ -21,12 +21,30 @@ class Kernel:
             map(lambda pin: Button(pin),
                 (12, 13, 14, 15) if not config.ROTATE else (15, 14, 13, 12)))
 
+        self.tasks = {}
+
         self.WIFI_SSID = config.WIFI_SSID
         self.WIFI_PASSWORD = config.WIFI_PASSWORD
         self.TIMEZONE = config.TIMEZONE
 
-    def add_task(self, *args):
-        task.add_task(*args)
+    def add_task(self, name, delay_ms, callback):
+        self.tasks[name] = {
+            'time': time.ticks_add(time.ticks_ms(), delay_ms),
+            'callback': callback
+        }
+
+    def exec_tasks(self, *args):
+        now_ms = time.ticks_ms()
+        ready = dict((k, v) for k, v in self.tasks.items()
+                     if time.ticks_diff(v['time'], now_ms) < 0)
+
+        for key, task in ready.items():
+            del self.tasks[key]
+            delay_ms = task['callback'](*args)
+            if delay_ms != None:
+                self.add_task(key, delay_ms, task['callback'])
+
+        return len(ready) > 0
 
     def wlan_connect(self, now=Clock.now()):
         self.display.show_status(now, 'connecting', 'wifi ...')
@@ -50,7 +68,7 @@ class Kernel:
         while True:
             now = Clock.now()
 
-            task_run = task.exec_tasks(now)
+            task_run = self.exec_tasks(now)
 
             pressed = list(map(lambda button: button.read(), self.buttons))
 
