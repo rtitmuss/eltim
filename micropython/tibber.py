d2a2fc78d413cec6f4f025f48234422695829b9a
Richard Titmuss <richardt@spotify.com>
2022-11-19 15:55:27 +0100

    Set level based on 7 day average price

--- micropython/tibber.py
+++ micropython/tibber.py
@@ -6,8 +6,14 @@ import array
 import urequests as requests
 
 _QUERY = const(
-    """{ viewer { homes { currentSubscription { priceInfo { current { startsAt } } priceRating { hourly { entries { total time level } } } } } } }"""
+    """{ viewer { homes { currentSubscription { priceInfo { current { startsAt } } priceRating { hourly { entries { total time } } } } } } }"""
 )
+
+
+def get_level(e, low_price, high_price):
+    e['level'] = 'LOW' if e['total'] < low_price else 'HIGH' if e[
+        'total'] > high_price else 'NORMAL'
+    return e
 
 
 def fetch_price_info(token):
@@ -25,8 +31,18 @@ def fetch_price_info(token):
         today = subscription['priceInfo']['current']['startsAt'][:10]
         price_rating = subscription['priceRating']['hourly']['entries']
 
+        price = list(map(lambda e: e['total'], price_rating))
+
+        min_price = min(price)
+        max_price = max(price)
+        avg_price = sum(price) / len(price)
+
+        low_price = min_price + (avg_price - min_price) / 2
+        high_price = avg_price + (max_price - avg_price) / 2
+
         today_tomorrow_rating = list(
-            filter(lambda e: e['time'] >= today, price_rating))
+            map(lambda e: get_level(e, low_price, high_price),
+                filter(lambda e: e['time'] >= today, price_rating)))
 
         return array.array('f', map(lambda e: e['total'], today_tomorrow_rating)), \
                list(map(lambda e: e['level'], today_tomorrow_rating))
